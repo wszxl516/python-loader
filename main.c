ae3fa762a912c5fa2077829404dedfda8e50ad80
Zhang <wszxl516@163.com>
2020-04-10 00:47:59 +0800

    clean env after run

--- main.c
+++ main.c
@@ -8,22 +8,31 @@ void mkenv()
 {
     int status;
     FILE *fp,*main;                                                                  
-    status = mkdir("/tmp/lib", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); 
-    fp = fopen("/tmp/lib/python36.zip", "wb");
+    status = mkdir("/tmp/python", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); 
+    status = mkdir("/tmp/python/lib", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH); 
+    fp = fopen("/tmp/python/lib/python36.zip", "wb");
     fwrite((void*)data, 1, sizeof(data)/sizeof(char), fp); 
     fclose(fp);
-    main = fopen("/tmp/__main__.py", "wb");
+    main = fopen("/tmp/python/__main__.py", "wb");
     fwrite((void*)main_data, 1, sizeof(main_data)/sizeof(char), main);
     fclose(main);
 
 }
+void cleanenv()
+{
+    remove("/tmp/python/lib/python36.zip");
+    remove("/tmp/python/lib/");
+    remove("/tmp/python/__main__.py");
+    remove("/tmp/python/");
+}
 
 int main(int argc, char **argv)
 {
-    char *filename="/tmp/__main__.py";
-    wchar_t *tmp = L"/tmp/lib/python36.zip";
-    wchar_t *path = L"/tmp";
+    char *filename="/tmp/python/__main__.py";
+    wchar_t *tmp = L"/tmp/python/lib/python36.zip";
+    wchar_t *path = L"/tmp/python";
     mkenv();
     runPython(argc, argv, filename, tmp, path);
+    cleanenv();
     return 0;
 }
